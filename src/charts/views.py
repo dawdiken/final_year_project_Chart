905bd20abc5706ee3226f8997ebc87441e9ede87
DAVID KENNY - Student <G00070718@gmit.ie>
2018-01-24 15:33:22 +0000

    database mysql to web page fix

--- src/charts/views.py
+++ src/charts/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 # from django.db.backends import mysql
 #import MySQLdb
 import pymysql
+import json
 
 
 
@@ -34,6 +35,8 @@ def homeView(request):
     cursor = conn.cursor()
     cursor.execute("select userName from users")
     rows = cursor.fetchall()
+    jsonliststuff = json.dumps(rows)
+    print (jsonliststuff)
     labels = []
     print(rows)
     for line in rows:
@@ -67,10 +70,33 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        labels = ["Des", "Mike", "Michelle", "Paul", "Brian", "Niall"]
+        conn = pymysql.connect(host="35.184.175.243",  # your host, usually localhost
+                               user="root",  # your username
+                               passwd="test12",  # your password
+                               db="engineering")  # name of the data base
+        try:
+            cursor = conn.cursor()
+            cursor.execute("select userName from users")
+            rows = cursor.fetchall()
+            jsonliststuff = json.dumps(rows)
+            print(jsonliststuff)
+            labels = []
+            print(rows)
+            for line in rows:
+                print(line)
+                for element in line:
+                    print(element)
+                    # labels.add()
+
+            # print(labels.all)
+        finally:
+            conn.close()
+
+        labels = [["Des"], ["Mike"], ["Michelle"], ["Paul"], ["Brian"], ["Niall"]]
+        print(labels)
         default_items = [10, 15, 17, 25, 9, 7]
         data = {
-            "labels": labels,
+            "labels": rows,
             "default": default_items,
 
         }
