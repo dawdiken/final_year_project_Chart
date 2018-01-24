7c24131ed8f0184ef8b73ee7a95d24a2d51e528e
DAVID KENNY - Student <G00070718@gmit.ie>
2018-01-24 14:56:30 +0000

    database mysql

--- src/charts/views.py
+++ src/charts/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 # from django.db.backends import mysql
-import MySQLdb
+#import MySQLdb
 import pymysql
 
 
